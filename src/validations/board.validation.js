d10db6ee63ea950fefa10d3c8ef5198e71e5ca78
FromSunNews <phuongtu590@gmail.com>
2022-10-19 19:44:24 +0700

    My trello-api

---
+++ src/validations/board.validation.js
@@ -0,0 +1,39 @@
+import Joi from 'joi'
+import { HttpStatusCode } from '*/utilities/constants'
+
+const createNew = async (req, res, next) => {
+  const condition = Joi.object({
+    title: Joi.string().required().min(1).max(50).trim()
+  })
+  try {
+    await condition.validateAsync(req.body, { abortEarly: false })
+    next()
+  } catch (error) {
+    res.status(HttpStatusCode.BAD_REQUEST).json({
+      errors: new Error(error).message
+    })
+  }
+}
+
+const update = async (req, res, next) => {
+  const condition = Joi.object({
+    title: Joi.string().min(1).max(50).trim(),
+    columnOrder: Joi.array().items(Joi.string())
+  })
+  try {
+    await condition.validateAsync(req.body, {
+      abortEarly: false,
+      allowUnknown: true
+    })
+    next()
+  } catch (error) {
+    res.status(HttpStatusCode.BAD_REQUEST).json({
+      errors: new Error(error).message
+    })
+  }
+}
+
+export const BoardValidation = {
+  createNew,
+  update
+}
